08a62f45854ba33f3d086c42ef49ec213800571e
diklakoo <diklakoo@gmail.com>
2020-12-08 11:19:31 +0200

    Division into functions

--- main_flower.py
+++ main_flower.py
@@ -1,18 +1,29 @@
-flowers = {}
-file= open("iris.data",'r')
-for line in file :
-    l = line.rstrip().split(",")
-    if len(l) == 5 :
-        height = l[3]
-        name = l[4]
-        if name in flowers:
-            flowers[name] = round(float(flowers[name]) + float(height), 2)
-        else:
-            flowers[name] = height
 
-def printing_flower_height(dict):
- for key in dict:
-    print ( key,dict[key])
+def Cumulative_height():
+    flowers = {}
+    file = open("iris.data", 'r')
+    for line in file:
+        l = line.rstrip().split(",")
+        if len(l) == 5:
+            height = l[3]
+            name = l[4]
+            if name in flowers:
+                flowers[name] = round(float(flowers[name]) + float(height), 2)
+            else:
+                flowers[name] = height
+    return flowers
 
-printing_flower_height(flowers)
+def printing_flower_height():
+    flowers = Cumulative_height()
+    for key in flowers:
+      print ( key,flowers[key])
+
+
+def main() :
+    printing_flower_height()
+
+if __name__ == '__main__':
+    main()
+
+
 
